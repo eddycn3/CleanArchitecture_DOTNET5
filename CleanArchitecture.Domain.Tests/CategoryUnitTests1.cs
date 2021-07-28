0bbdc7794ce5469e1c90724809a66e15291efa7e
eduardo c neto <eddycn3@gmail.com>
2021-07-28 15:40:03 -0300

    feat: domain unit testes

---
+++ CleanArchitecture.Domain.Tests/CategoryUnitTests1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using CleanArchitecture.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace CleanArchitecture.Domain.Tests
+{
+    public class CategoryUnitTests1
+    {
+        [Fact]
+        public void CreateCategory_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Category(1, "Category Name");
+            action.Should()
+                .NotThrow<CleanArchitecture.Domain.Validation.DomainExceptionValidation>();
+
+        }
+
+        [Fact]
+        public void CreateCategory_WithInvalidParameters_DomainExceptionInvalidId()
+        {
+            Action action = () => new Category(-1, "Category Name");
+            action.Should()
+                .Throw<CleanArchitecture.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid id value");
+        }
+
+        [Fact]
+        public void CreateCategory_WithNullOrEmptyValue_DomainExceptionNameIsRequired()
+        {
+            Action action = () => new Category(null);
+            action.Should()
+                .Throw<CleanArchitecture.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Category name is required, cannot be null or empty");
+           
+        }
+
+
+        [Fact]
+        public void CreateCategory_WithShortLenth_DomainExceptionNameIsToShort()
+        {
+            Action action = () => new Category("Ca");
+            action.Should()
+                .Throw<CleanArchitecture.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Category name to short, minimum is 3 characters");
+        }
+    }
+}
---
+++ CleanArchitecture.Domain.Tests/ProductUnitTestes1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using CleanArchitecture.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace CleanArchitecture.Domain.Tests
+{
+    public class ProductUnitTestes1
+    {
+        [Fact]
+        public void CreateProduct_WithValidParameters_ReturnObjectValidState()
+        {
+            Action action = () => new Product(1,"Product", "Dddddd", 9m, 1, "qwdqwdqwdqwd");
+            action.Should()
+                .NotThrow<CleanArchitecture.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateProduct_WithInvalidId_DomainExceptionInvalidId()
+        {
+            Action action = () =>  new Product(-1,"Product", "Dddddd", 9m, 1, "qwdqwdqwdqwd");
+            action.Should()
+                .Throw<CleanArchitecture.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Product Id");
+        }
+
+        [Fact]
+        public void CreateProduct_WithNullOrEmptyName_DomainExceptionNameCannotBeNullOrEmpty()
+        {
+            Action action = () =>  new Product(1, null, "Dddddd", 9m, 1, "qwdqwdqwdqwd");
+            action.Should()
+                .Throw<CleanArchitecture.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Product name cannot be null or empty");
+
+        }
+
+        [Fact]
+        public void CreateProduct_WithShortName_DomainExceptionNameToShort()
+        {
+            Action action = ()=> new Product(1, "Pr", "Dddddd", 9m, 1, "qwdqwdqwdqwd");
+            action.Should()
+                .Throw<CleanArchitecture.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Product name to short, min lenght is 3");
+        }
+
+        [Fact]
+        public void CreateProduct_WithEmptyOrNullDescription_DomainExceptionDescriptionIsNullOrEmpty()
+        {
+            Action action = () =>  new Product(1, "Product", null, 9m, 1, "qwdqwdqwdqwd");
+            action.Should()
+                .Throw<CleanArchitecture.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Product description cannot be null or empty");
+        }
+
+        [Fact]
+        public void CreateProduct_WithShortDescription_DomainExceptionDescriptionIsToShort()
+        {
+            Action action = () => new Product(1, "Product", "Dddd", 9m, 1, "qwdqwdqwdqwd");
+            action.Should()
+                .Throw<CleanArchitecture.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Product description to short, min lenght is 5");
+        }
+
+        [Fact]
+        public void CreateProduct_WithInvalidProductPrice_DomainExceptionInvalidProductPrice()
+        {
+            Action action = () => new Product(1, "Produc", "Ddddd", -1, 1, "qwdqwdqwdqwd");
+            action.Should()
+                .Throw<CleanArchitecture.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid product price");
+        }
+
+        [Fact]
+        public void CreateProduct_WithInvalidProductStock_DomainExceptionInvalidProductStock()
+        {
+            Action action = () => new Product(1, "Produc", "Ddddd", 9m, -1, "qwdqwdqwdqwd");
+            action.Should()
+                .Throw<CleanArchitecture.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid product stock");
+        }
+
+
+        [Fact]
+        public void CreateProduct_WithProductImageToLong_DomainExceptionImageLenthToLong()
+        {
+            Action action = () => new Product(1, "Product", "Dddddd", 9m, 1, "qwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdvqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwdqwd");
+            action.Should()
+                .Throw<CleanArchitecture.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Product image to long, max length is 250");
+        }
+
+
+        [Fact]
+        public void CreateProduct_WithProductNullImageValue_NotDomainException()
+        {
+            Action action = () => new Product(1, "Product", "Dddddd", 9m, 1,null);
+            action.Should()
+                .NotThrow<CleanArchitecture.Domain.Validation.DomainExceptionValidation>();
+               
+        }
+
+        [Fact]
+        public void CreateProduct_WithProductNullImageValue_NotNullReferenceException()
+        {
+            Action action = () => new Product(1, "Product", "Dddddd", 9m, 1, null);
+            action.Should()
+                .NotThrow<NullReferenceException>();
+
+        }
+
+    }
+}
